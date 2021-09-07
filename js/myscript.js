7474a6677b3732a11a61079ba169f2d4e8a3a6d7
Gabrielecorti01 <gcorti.info@gmail.com>
2021-09-07 17:44:58 +0200

    ESERCIZIO TERMINATO

--- js/myscript.js
+++ js/myscript.js
@@ -6,17 +6,44 @@
 // Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto
 // studente inserendo nell’ordine: nome, cognome e età.
 
-
-let nomiStudenti = ["Gabriele", "Walter", "Eugenio", "Sofia", "Morena"];
-let cognomiStudenti = ["Corti", "Frongia", "Franzoso","Papa", "Fischione"];
-let etaStudenti = ["21", "22", "23", "24", "25"];
-
-let studente = {
-    nome: "",
-    cognome: "",
-    eta: "",
+let listaStudenti = [
+    {
+        nome: "Mario",
+        cognome: "Rossi",
+        eta: "23"
+    },
+    {
+        nome: "Luca",
+        cognome: "Giallo",
+        eta: "24",
+    },
+    {
+        nome: "Anna",
+        cognome: "Blu",
+        eta: "43",
+    }
+]
+for (let i = 0; i < listaStudenti.length; i++){
+    console.log("Studente: " + listaStudenti[i]["nome"], listaStudenti[i].cognome)
 }
 
+let studenteScelto = {
+    nome: prompt("Inserisci un nome!"),
+    cognome: prompt("inserisci un cognome!"),
+    eta: prompt("inserisci l'eta!"),
+}
+listaStudenti.push(studenteScelto)
+console.log("studente: " + studenteScelto["nome"] + " " + studenteScelto["cognome"])
+
+// Creo un oggetto che descriva lo studente
+let studente = {
+    nome: "Gabriele",
+    cognome: "Corti",
+    eta: "20"
+}
+
+// Stampo a schermo l'oggetto dello studente
 for (let proprieta in studente){
-    console.log(proprieta);
+
+    console.log(proprieta + ": " + studente[proprieta])
 }
